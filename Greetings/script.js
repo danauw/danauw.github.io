b88ea78334f5a7b257a8bc9a03c25522d02d5132
Petter Amaleona <prodevgy6@gmail.com>
2021-10-10 08:12:21 +0800

    Added birthday greetings

---
+++ Greetings/script.js
@@ -0,0 +1,77 @@
+(function () {
+  function $(id) {
+    return document.getElementById(id);
+  }
+  function isLightColor(color) {
+    const hex = color.replace("#", "");
+    const c_r = parseInt(hex.substr(0, 2), 16);
+    const c_g = parseInt(hex.substr(2, 2), 16);
+    const c_b = parseInt(hex.substr(4, 2), 16);
+    const brightness = (c_r * 299 + c_g * 587 + c_b * 114) / 1000;
+    return brightness > 155;
+  }
+
+  const card = $("card"),
+    openB = $("open"),
+    closeB = $("close")
+    cardFront = $("card-front"),
+    coverTitle = $("cover-title"),
+    cardInside = $("card-inside"),
+    randomColor = Math.floor(Math.random() * 16777215).toString(16),
+    timer = null,
+    fontList = [
+      '\'Nobile\', sans-serif',
+      '\'Dancing Script\', cursive',
+      '\'Ephesis\', cursive',
+      '\'Explora\', cursive',
+      '\'Gemunu Libre\', sans-serif',
+      '\'Pacifico\', cursive',
+      '\'Architects Daughter\', cursive',
+      '\'Kaushan Script\', cursive',
+      '\'Itim\', cursive',
+      '\'Rock Salt\', cursive'
+    ],
+    font = fontList[Math.floor(Math.random()*fontList.length)];
+
+  cardFront.style.backgroundColor = randomColor;
+  openB.style.backgroundColor = randomColor;
+  closeB.style.backgroundColor = randomColor;
+  openB.style.color = '#fff';
+  closeB.style.color = '#fff';
+  coverTitle.style.color = '#fff';
+
+
+  // apply random font
+  coverTitle.style.fontFamily = font;
+  cardInside.style.fontFamily = font;
+  openB.style.fontFamily = font;
+  closeB.style.fontFamily = font;
+
+
+  if (isLightColor(randomColor)) {
+    openB.style.color = '#000';
+    closeB.style.color = '#000';
+    coverTitle.style.color = '#000';
+  }
+  const confettiSettings = { target: 'confetti' };
+  const confetti = new ConfettiGenerator(confettiSettings);
+  confetti.render();
+
+  openB.addEventListener("click", function () {
+    card.setAttribute("class", "open-half");
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute("class", "open-fully");
+      timer = null;
+    }, 500);
+  });
+
+  closeB.addEventListener("click", function () {
+    card.setAttribute("class", "close-half");
+    if (timer) clearTimerout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute("class", "");
+      timer = null;
+    }, 500);
+  });
+})();
