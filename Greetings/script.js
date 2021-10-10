7efadddb47723e8bb5a33e366a005ce524939890
Petter Amaleona <prodevgy6@gmail.com>
2021-10-10 19:06:31 +0800

    Added audio, greetings

--- Greetings/script.js
+++ Greetings/script.js
@@ -11,18 +11,19 @@
     return brightness > 155;
   }
 
+  let timer = null;
   const card = $("card"),
     openB = $("open"),
-    closeB = $("close")
+    closeB = $("close"),
     cardFront = $("card-front"),
     coverTitle = $("cover-title"),
     cardInside = $("card-inside"),
-    message = $("message")
+    message = $("message"),
+    closingRemark = $("closing-remark"),
     from = $("from"),
     prev = $("prev"),
     next = $("next"),
     randomColor = Math.floor(Math.random() * 16777215).toString(16),
-    timer = null,
     fontList = [
       '\'Nobile\', sans-serif',
       '\'Dancing Script\', cursive',
@@ -35,9 +36,20 @@
       '\'Itim\', cursive',
       '\'Rock Salt\', cursive'
     ],
+    remarks = [
+      'Yours truly',
+      'Sincerely',
+      'Regards',
+      'Best regards',
+      'Warm regards',
+      'Kind regards',
+      'Best of luck'
+    ],
     font = fontList[Math.floor(Math.random()*fontList.length)],
+    remark = remarks[Math.floor(Math.random()*remarks.length)]
     froms = [{
       name: 'Mikki',
+      alias: '선생님 / 오빠 Mikki',
       message: "I'll make it simple, wishing you all the best. Be strong through tough times stay optimistic, surround yourself with people that will care and love you, stay healthy, be successful in your career path and lastly enjoy life being 18 ( I did not include be beautiful because you already are hahaha). Happy Birthday"
     }, {
       name: 'Kysha',
@@ -66,6 +78,54 @@
     }, {
       name: 'Flores.jayson',
       message: "Happy 18th birthday !! I wish you all the best and stay kind, always take care of yourself and be healthy. Goodluck on your journey there in korea and hope that you will be kind to Lamoks."
+    }, {
+      name: 'a.john_lng',
+      message: "happy birthday dana, even though i am not as active as i was before because i am very busy with my studies, i want to greet you and hope you have a great birthday and a long life. keep safe always, stay healthy. we support you dana. happy birthday again"
+    }, {
+      name: 'Jiv',
+      message: "Hi Dana! It’s Jiv here. Happy Happy Birthday to you!! My birthday is also coming up so greet me too okay? HAHAHAHA Anyways, stay safe always and please take good care of yourself, physically and mentally. Let’s meet when I go to Korea soon okay? Have a good one! &#10084;&#129346;"
+    }, {
+      name: 'Royandot',
+      message: "stay young, stay pretty, stay humble focus to her study and happy happy birthday to Dana, enjoy your birthday"
+    }, {
+      name: 'Harris',
+      message: "I just first wanna tell you to takecare of yourself. I hope your special day will bring you lots of happiness and bring every success. I wish nothing but good things on your birthday. Wishing you many more years of good health and prosperity and all the most amazing things on your birthday. May your birthday today(tomorrow) bring you a step closer to realizing every dream and desire of yours and may your future (dream) always shine like a candle on your birthday cake.\n\nHappy Birthday dana &#128522;&#10084;"
+    }, {
+      name: 'Roland',
+      message: "Happy birthday Dana, I hope you're always safe and healthy, I hope your behavior doesn't change, you're cute, you're beautiful, and I hope I'll meet you soon I hope to find the one who will truly love you first, the one who will take care of you and be with you for life from"
+    }, {
+      name: 'Roman',
+      message: "To my only ANGEL that rescued me from the fate of doomness:\n\n \
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Before I met you here on the internet I was wondering if I would ever see a beaufitul Angel that can comfort me in every step of the way. \
+      What I saw was nothing but loneliness not just personally but also deep inside of my HEART. There were many moments that I was totally sad because I can not even comfort my ownself, I think I need someone that can be my comforter. \
+      There are also the moments that I was thinking to end my life negatively on every aspects. I thought to myself that I may not bare it at all. Every day, I sometimes think of ending it too soon but I waited patiently for the Angel to come.  \
+      Even if I do not meet her but I know that she will exist in this chaotic world full of sins. I go deeper into the internet indulging with music that made me comfortable. \
+      I did not think too much of a happy thoughts I was glad for the time being but it did not take much long enough and then it fades away like a leaf or dust where it dissolves easily and never to be seen again. \
+      For every aspects of my life there was these thoughts that should not and must not materialized or else I will stay in that way. \n\n \
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;For almost two (2) years I suffer a lot with my own health deteroriating in all aspects. I stayed mentally stress and physically stressed. \
+      I need to enjoy a much longer lifespan with someone in my life whoever she is that can comfort me in every way. \
+      For almost two (2) years I suffer a lot with my own health deteroriating in all aspects. I stayed mentally stress and physically stressed. \
+      I need to enjoy a much longer lifespan with someone in my life whoever she is that can comfort me in every way. \
+      For almost two (2) years I sometimes enjoy watching and reading but the happiness in it fades away so easily. \
+      Then I found this guy Marcus whom you encounter in OmeTV but before you encountered him on the public platform his videos were comforting me for a period of a time and so again it simply fades away in the vast emptiness of space. \
+      Until 1 Day I saw leaked from his 700K LIVESTREAM that brings me to your Instagram and then I followed you even if you do not follow me back I will still continue supporting you in every way I can. \
+      Now that we can communicate with each other specially to the other Mates that has also been there for you and keep you safe from harm. We are always there in case you need a helping hand we can be the best that you want and need to be.\n\n \
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You have never encountered me yet on OmeTV but when the time comes you will notice what I look like and what my voice is but not in a cartoonic voice and character and personality I am not like that. I am an introvert. \
+      You have emboldened my heart in a softer ways. You have made me happy in every aspects of my life and I am very fond of you especially that you have charming personality and character that attracts my attention in every way. \
+      Now that you are turning 18 we will celebrate it like non-stop. I always think of happy thoughts in the lightness of every elements. \
+      You will have my utmost support in chasing your dreams. You will never know one (1) day you will encounter me and then I could be the one promoting your dreams since I have a futuristic spirit inside of me. \
+      I wish you and bless you with your sister on your respective 15th Year of Existence and 18th Year of Existence. \n\n \
+      <a href=\"#\">#MYHAPPYPILLDana</a> \n \
+      <a href=\"#\">#TheAngelThatSavedME</a> \n \
+      <a href=\"#\">#HAPPY18thBIRTHDAY</a> \n \
+      <a href=\"#\">#WelcometotheClubofLegalAge</a> \n \
+      <a href=\"#\">#ANewLIFE</a> \n \
+      <a href=\"#\">#ABetterFuture</a> \n \
+      <a href=\"#\">#TheAngelThatChangedMEfortheBest</a> \n \
+      <a href=\"#\">#ForDANA</a>"
+    }, {
+      name: 'Godwin',
+      message: '<iframe width="560" height="315" src="https://www.youtube.com/embed/wZou218BL4o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
     }];
 
   const urlParams = new URLSearchParams(window.location.search);
@@ -96,7 +156,8 @@
   cardInside.style.fontFamily = font;
 
   // apply message
-  from.innerHTML = user && '- ' + user.name;
+  from.innerHTML = user && (user.alias ? user.alias : user.name);
+  closingRemark.innerHTML = remark + ','
   message.innerHTML = user && user.message.replace(/\n/g, "<br />");
 
   // update prev url
